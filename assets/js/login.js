aca2bd6b8c1677638a53e397ce3b1b7f65abd46f
wangjin <wangjin@qq.com>
2021-01-07 11:39:14 +0800

    修改

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function() {
+    // 点击去注册
+    $('#link-log').click(function() {
+        $('.login-box').hide()
+        $('.register-box').show()
+    })
+    $('#link-reg').click(function() {
+        $('.register-box').hide()
+        $('.login-box').show()
+    })
+
+})
